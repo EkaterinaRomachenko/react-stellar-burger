78bc81e1085e798cccc62aa3b7dae115b6a25ce9
Екатерина Ромащенко <129534521+EkaterinaRomachenko@users.noreply.github.com>
2023-09-23 09:27:56 +0200

    initial state of the project

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,6 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+// import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
 
 function App() {
   return (
